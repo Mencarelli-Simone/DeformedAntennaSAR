error in dummyPatterns.py papplied phase gradient corrected. Thanks ACMA

dummyPatterns.py
@@ -14,7 +14,9 @@ import matplotlib.pyplot as plt
 from ffsFileWriter import ffsWrite
 
 # %% user input
-
+# output filenames
+reference = 'dummyReference.ffs'
+distort = 'dummyDistorted.ffs'
 # antenna length
 L = 2  # m
 # antenna width
@@ -30,7 +32,7 @@ antenna = UniformAperture(L, W)
 antenna.set_uniform_mesh_resolution(1 / 3 * antenna.c / 10e9, 1 / 3 * antenna.c / 10e9)
 # set the field to uniform illumination (phase and amplitude)
 antenna.tanEField = np.ones_like(antenna.tanEField)
-# create a theta phi meshgrid ( just the positive emisphere is fine)
+# create a theta phi meshgrid ( just the positive hemisphere is fine)
 theta = np.linspace(0, np.pi / 2, 1801)
 phi = np.linspace(0, 2 * np.pi, 1801)
 T, P = np.meshgrid(theta, phi)
@@ -51,7 +53,7 @@ print(gmax - np.max(gain))
 # %% save this to a ffs file
 ffsWrite(T.reshape(-1) * 180 / np.pi, P.reshape(-1) * 180 / np.pi,
          E_theta.reshape(-1), E_phi.reshape(-1), len(phi), len(theta),
-         'dummyReference.ffs',
+         reference,
          radiated_power=antenna.get_radiated_power(),
          accepted_power=antenna.get_radiated_power(),
          stimulated_power=antenna.get_radiated_power(),
@@ -64,24 +66,24 @@ fig, ax = plt.subplots(1)
 
 # H cut
 th = np.linspace(-np.pi / 30, np.pi / 30, 501)
-ph = np.array(1) * np.pi/2
+ph = np.array(1) * np.pi / 2
 t, p = np.meshgrid(th, ph)
 E_theta, E_phi = antenna.mesh_E_field(t, p, polarization='y')  # H pol for the radar
 # equation 18.6.10 Orfanidis - Electromagnetic Waves and Antennas
 gain = 2 * np.pi * (np.abs(E_theta) ** 2 + np.abs(E_phi) ** 2) / (antenna.eta * antenna.get_radiated_power())
 
-ax.plot(t.reshape(-1)*180/np.pi, gain.reshape(-1))
+ax.plot(t.reshape(-1) * 180 / np.pi, gain.reshape(-1))
 ax.set_xlabel('$\Theta$ [deg]')
 ax.set_ylabel('Gain')
 ax.set_ylim(0, 8500)
 # E cut
-#th = np.linspace(-np.pi / 2, np.pi / 2, 501)
+# th = np.linspace(-np.pi / 2, np.pi / 2, 501)
 ph = np.array(1) * 0
 t, p = np.meshgrid(th, ph)
 E_theta, E_phi = antenna.mesh_E_field(t, p, polarization='y')  # H pol for the radar
 # equation 18.6.10 Orfanidis - Electromagnetic Waves and Antennas
 gain = 2 * np.pi * (np.abs(E_theta) ** 2 + np.abs(E_phi) ** 2) / (antenna.eta * antenna.get_radiated_power())
-ax.plot(t.reshape(-1)*180/np.pi, gain.reshape(-1))
+ax.plot(t.reshape(-1) * 180 / np.pi, gain.reshape(-1))
 plt.show()
 
 
@@ -103,7 +105,7 @@ alpha = L / float(r)
 # length shrinking
 L1 = np.sin(alpha) * r
 # wavelength dispacement
-phase = lambda y: ((r - np.sqrt(r ** 2 - (L1 / 2 - y) ** 2)) / (3e8 / 1e10)) % (2 * np.pi)
+phase = lambda y: ((r - np.sqrt(r ** 2 - (L1 / 2 - y) ** 2)) * 2 * np.pi / (3e8 / 1e10)) % (2 * np.pi)
 
 # new antenna
 antenna = UniformAperture(L1, W)
@@ -112,9 +114,12 @@ antenna.set_uniform_mesh_resolution(1 / 3 * 3e8 / 10e9, 1 / 3 * 3e8 / 10e9)
 # set the field to phase shifted  illumination (phase and amplitude)
 Y, X = np.meshgrid(antenna.l_mesh, antenna.w_mesh)
 antenna.tanEField = np.ones_like(antenna.tanEField) * np.exp(1j * phase(Y))
-
+#%%
 fig, ax = plt.subplots(1)
-ax.pcolormesh(Y, X, np.angle(antenna.tanEField))
+c = ax.pcolormesh(Y, X, np.angle(antenna.tanEField))
+plt.colorbar(c, ax=ax, label='[radians]')
+ax.set_xlabel('y [m]')
+ax.set_ylabel('x [m]')
 plt.show()
 
 # %% compute the gain ( is going to be slow)
@@ -134,7 +139,7 @@ print(gmax - np.max(gain))
 # %% save this to a ffs file
 ffsWrite(T.reshape(-1) * 180 / np.pi, P.reshape(-1) * 180 / np.pi,
          E_theta.reshape(-1), E_phi.reshape(-1), len(phi), len(theta),
-         'dummyDistorted.ffs',
+         distort,
          radiated_power=antenna.get_radiated_power(),
          accepted_power=antenna.get_radiated_power(),
          stimulated_power=antenna.get_radiated_power(),
@@ -146,22 +151,24 @@ fig, ax = plt.subplots(1)
 
 # H cut
 th = np.linspace(-np.pi / 30, np.pi / 30, 501)
-ph = np.array(1) * np.pi/2
+ph = np.array(1) * np.pi / 2
 t, p = np.meshgrid(th, ph)
 E_theta, E_phi = antenna.mesh_E_field(t, p, polarization='y')  # H pol for the radar
 # equation 18.6.10 Orfanidis - Electromagnetic Waves and Antennas
 gain = 2 * np.pi * (np.abs(E_theta) ** 2 + np.abs(E_phi) ** 2) / (antenna.eta * antenna.get_radiated_power())
 
-ax.plot(t.reshape(-1)*180/np.pi, gain.reshape(-1))
+ax.plot(t.reshape(-1) * 180 / np.pi, gain.reshape(-1))
 ax.set_xlabel('$\Theta$ [deg]')
 ax.set_ylabel('Gain')
 ax.set_ylim(0, 8500)
 # E cut
-#th = np.linspace(-np.pi / 2, np.pi / 2, 501)
+# th = np.linspace(-np.pi / 2, np.pi / 2, 501)
 ph = np.array(1) * 0
 t, p = np.meshgrid(th, ph)
 E_theta, E_phi = antenna.mesh_E_field(t, p, polarization='y')  # H pol for the radar
 # equation 18.6.10 Orfanidis - Electromagnetic Waves and Antennas
 gain = 2 * np.pi * (np.abs(E_theta) ** 2 + np.abs(E_phi) ** 2) / (antenna.eta * antenna.get_radiated_power())
-ax.plot(t.reshape(-1)*180/np.pi, gain.reshape(-1))
+ax.plot(t.reshape(-1) * 180 / np.pi, gain.reshape(-1))
 plt.show()
+
+